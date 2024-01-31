a92084050019bc5b692aebc6295499356401f4f2
Markus Walter <markus.walter93@gmx.de>
2024-01-31 15:42:01 +0000

    added duaration field

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -12,15 +12,36 @@ entries_collection = db['entries']
 # Routes
 @app.route('/')
 def index():
-    entries = entries_collection.find().sort('timestamp', -1)
+    
+    
+    today = datetime.combine(datetime.utcnow().date(), datetime.min.time())
+    end_of_day = today + timedelta(days=1)
+
+ #   entries = entries_collection.find().sort('timestamp', -1)
+    entries = entries_collection.find({
+    'timestamp': {'$gte': today, '$lt': end_of_day}
+    })
     return render_template('index.html', entries=entries)
+
+# @app.route('/add_entry', methods=['POST'])
+# def add_entry():
+#     content = request.form.get('content')
+#     timestamp = datetime.now()
+#     entry = {'content': content, 'timestamp': timestamp}
+#     entries_collection.insert_one(entry)
+#     return redirect(url_for('index'))
+
 
 @app.route('/add_entry', methods=['POST'])
 def add_entry():
     content = request.form.get('content')
+    duration = request.form.get('duration')  # Added line to retrieve duration
     timestamp = datetime.now()
-    entry = {'content': content, 'timestamp': timestamp}
+    
+    # Save the entry to MongoDB
+    entry = {'content': content, 'duration': duration, 'timestamp': timestamp}
     entries_collection.insert_one(entry)
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
